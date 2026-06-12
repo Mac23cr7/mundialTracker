ecceb7e2f1a9fc7074354cf9ef232aca7dc2319e
macvargas <mvargas@darient.com>
2026-06-12 11:04:58 -0400

    fix: Vercel deployment and static files collection configuration

--- sports_tracker/settings.py
+++ sports_tracker/settings.py
@@ -138,7 +138,7 @@ RUNNING_TESTS = 'test' in sys.argv
 if DEBUG or RUNNING_TESTS:
     STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 else:
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
